47cdd551c24eb332ac202e32a6b78af04ff70e82
Sander Vanneset <sander.vanneste@sweetmustard.be>
2018-09-17 14:27:08 +0200

    Option for using default container

--- src/module/files/__name@dasherize@if-flat__/__name@dasherize__.module.ts
+++ src/module/files/__name@dasherize@if-flat__/__name@dasherize__.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';<% if (commonModule) { %>
 import { CommonModule } from '@angular/common';<% } %><% if (routing) { %>
 
 import { <%= classify(name) %>RoutingModule } from './<%= dasherize(name) %>-routing.module';<% } %>
-import { <%= classify(name) %>Container } from './containers/<%= dasherize(name) %>-container/<%= dasherize(name) %>.container';
+<% if (container) { %>import { <%= classify(name) %>Container } from './containers/<%= dasherize(name) %>-container/<%= dasherize(name) %>.container'; <% } %>
 
 @NgModule({
   imports: [<% if (commonModule) { %>
     CommonModule<%= routing ? ',' : '' %><% } %><% if (routing) { %>
     <%= classify(name) %>RoutingModule<% } %>
   ],
-  declarations: [ <%= classify(name) %>Container ]
+  declarations: [ <% if (container) { %><%= classify(name) %>Container <% } %> ]
 })
 export class <%= classify(name) %>Module { }
