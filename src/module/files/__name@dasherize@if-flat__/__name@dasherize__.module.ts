9c813e947ee7ec99f49a3c934e0f715215625d88
Sander Vanneset <sander.vanneste@sweetmustard.be>
2018-09-17 14:09:03 +0200

    Upgrades

--- src/module/files/__name@dasherize@if-flat__/__name@dasherize__.module.ts
+++ src/module/files/__name@dasherize@if-flat__/__name@dasherize__.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';<% if (commonModule) { %>
 import { CommonModule } from '@angular/common';<% } %><% if (routing) { %>
 
 import { <%= classify(name) %>RoutingModule } from './<%= dasherize(name) %>-routing.module';<% } %>
+import { <%= classify(name) %>Container } from './containers/<%= dasherize(name) %>-container/<%= dasherize(name) %>.container';
 
 @NgModule({
   imports: [<% if (commonModule) { %>
     CommonModule<%= routing ? ',' : '' %><% } %><% if (routing) { %>
     <%= classify(name) %>RoutingModule<% } %>
   ],
-  declarations: []
+  declarations: [ <%= classify(name) %>Container ]
 })
 export class <%= classify(name) %>Module { }
