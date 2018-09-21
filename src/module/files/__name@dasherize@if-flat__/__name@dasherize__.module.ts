bf068eb093306545c44f85ba35d621ed8be18baa
Sander Vanneset <sander.vanneste@sweetmustard.be>
2018-09-21 16:23:36 +0200

    layout

--- src/module/files/__name@dasherize@if-flat__/__name@dasherize__.module.ts
+++ src/module/files/__name@dasherize@if-flat__/__name@dasherize__.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';<% if (commonModule) { %>
-import { CommonModule } from '@angular/common';<% } %><% if (routing) { %>
+import { NgModule } from '@angular/core'; <% if (commonModule) { %>
+import { CommonModule } from '@angular/common'; <% } %> <% if (routing) { %>
 
-import { <%= classify(name) %>RoutingModule } from './<%= dasherize(name) %>-routing.module';<% } %>
+import { <%= classify(name) %>RoutingModule } from './<%= dasherize(name) %>-routing.module'; <% } %>
 <% if (container) { %>import { <%= classify(name) %>Container } from './containers/<%= dasherize(name) %>-container/<%= dasherize(name) %>.container'; <% } %>
+<% if (sandbox) { %>import { <%= classify(name) %>Sandbox } from './sandbox/<%= dasherize(name) %>.sandbox'; <% } %>
 
 @NgModule({
-  imports: [<% if (commonModule) { %>
-    CommonModule<%= routing ? ',' : '' %><% } %><% if (routing) { %>
-    <%= classify(name) %>RoutingModule<% } %>
+    imports: [<% if(commonModule) { %>
+        CommonModule <%= routing ? ',' : '' %> <% } %> <% if (routing) { %>
+            <%= classify(name) %>RoutingModule <% } %>
   ],
-  declarations: [ <% if (container) { %><%= classify(name) %>Container <% } %> ]
+  declarations: [<% if (container) { %> <%= classify(name) %>Container <% } %> ],
+  providers: [<% if(sandbox) { %> <%= classify(name)%>Sandbox <% } %> ]
 })
 export class <%= classify(name) %>Module { }
