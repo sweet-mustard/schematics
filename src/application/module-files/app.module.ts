f4b1bf8afd03535aa6ec2ed2e50010312c0d3495
Sander Vanneset <sander.vanneste@sweetmustard.be>
2018-10-15 15:32:11 +0200

    App module changed when making a new application

--- src/application/module-files/app.module.ts
+++ src/application/module-files/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 <% if (routing) { %>
 import { AppRoutingModule } from './app-routing.module';<% } %>
-import { AppComponent } from './app.component';
+import { AppComponent } from './containers/root/app.component';
 
 @NgModule({
   declarations: [
