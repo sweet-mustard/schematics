bfee4e4b9bc281bb9b03abc471b8f6480ee98949
Sander Vanneset <sander.vanneste@sweetmustard.be>
2018-10-26 13:32:02 +0200

    Gitignore updated for js files

---
+++ src/workspace/files/prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 140, 
+    tabWidth: 2,    
+    useTabs: false,  
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'none', 
+    noBracketSpacing: true,
+    arrowParens: 'avoid',  
+    parser: 'typescript'
+  };
