c4113e68ee608da6f26ba3803a5dbfe3511fb110
Sander Vanneste <sander.vanneste@sweetmustard.be>
2019-12-01 21:15:43 +0100

    Update ng-new to support and use angular 8 features

--- src/ng-new/index.spec.ts
+++ src/ng-new/index.spec.ts
@@ -1,63 +1,63 @@
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import { Schema as NgNewOptions } from './schema';
 
-
 describe('Ng New Schematic', () => {
-    const schematicRunner = new SchematicTestRunner(
-        '@sweet-mustard/schematics',
-        require.resolve('../collection.json'),
-    );
-    const defaultOptions: NgNewOptions = {
-        name: 'foo',
-        directory: 'bar',
-        version: '6.0.0',
+  const schematicRunner = new SchematicTestRunner('@sweet-mustard/schematics', require.resolve('../collection.json'));
+  const defaultOptions: NgNewOptions = {
+    name: 'foo',
+    directory: 'bar',
+    version: '6.0.0'
+  };
+
+  it('should create files of a workspace', async () => {
+    const options = { ...defaultOptions };
+
+    const tree = await schematicRunner.runSchematicAsync('ng-new', options).toPromise();
+    const files = tree.files;
+    expect(files).toContain('/bar/angular.json');
+  });
+
+  it('should create files of an application', async () => {
+    const options = { ...defaultOptions };
+
+    const tree = await schematicRunner.runSchematicAsync('ng-new', options).toPromise();
+    const files = tree.files;
+    expect(files).toContain([
+      '/bar/src/tsconfig.app.json',
+      '/bar/src/main.ts',
+      '/bar/src/app/app.module.ts',
+      '/bar/e2e/src/app.po.ts',
+      '/bar/e2e/src/app.e2e-spec.ts',
+      '/bar/e2e/tsconfig.json',
+      '/bar/e2e/protractor.conf.js'
+    ]);
+  });
+
+  it('should should set the prefix in angular.json and in app.component.ts', async () => {
+    const options = { ...defaultOptions, prefix: 'pre' };
+
+    const tree = await schematicRunner.runSchematicAsync('ng-new', options).toPromise();
+    const content = tree.readContent('/bar/angular.json');
+    expect(content).toMatch(/"prefix": "pre"/);
+  });
+
+  it('should set up the app module', async () => {
+    const options: NgNewOptions = {
+      name: 'foo',
+      version: '6.0.0'
     };
 
-    it('should create files of a workspace', () => {
-        const options = { ...defaultOptions };
+    const tree = await schematicRunner.runSchematicAsync('ng-new', options).toPromise();
+    const moduleContent = tree.readContent('/foo/src/app/app.module.ts');
+    expect(moduleContent).toMatch(/declarations:\s*\[\s*RootContainer\s*\]/m);
+  });
 
-        const tree = schematicRunner.runSchematic('ng-new', options);
-        const files = tree.files;
-        expect(files).toContain('/bar/angular.json');
-    });
+  it('createApplication=false should create an empty workspace', async () => {
+    const options = { ...defaultOptions, createApplication: false };
 
-    it('should create files of an application', () => {
-        const options = { ...defaultOptions };
-
-        const tree = schematicRunner.runSchematic('ng-new', options);
-        const files = tree.files;
-        expect(files).toEqual(jasmine.arrayContaining([
-            '/bar/src/tsconfig.app.json',
-            '/bar/src/main.ts',
-            '/bar/src/app/app.module.ts',
-        ]));
-    });
-
-    it('should should set the prefix in angular.json and in app.component.ts', () => {
-        const options = { ...defaultOptions, prefix: 'pre' };
-
-        const tree = schematicRunner.runSchematic('ng-new', options);
-        const content = tree.readContent('/bar/angular.json');
-        expect(content).toMatch(/"prefix": "pre"/);
-    });
-
-    it('should set up the app module', () => {
-        const options: NgNewOptions = {
-            name: 'foo',
-            version: '6.0.0',
-        };
-
-        const tree = schematicRunner.runSchematic('ng-new', options);
-        const moduleContent = tree.readContent('/foo/src/app/app.module.ts');
-        expect(moduleContent).toMatch(/declarations:\s*\[\s*RootContainer\s*\]/m);
-    });
-
-    it('createApplication=false should create an empty workspace', () => {
-        const options = { ...defaultOptions, createApplication: false };
-
-        const tree = schematicRunner.runSchematic('ng-new', options);
-        const files = tree.files;
-        expect(files).toContain('/bar/angular.json');
-        expect(files).not.toContain('/bar/src');
-    });
+    const tree = await schematicRunner.runSchematicAsync('ng-new', options).toPromise();
+    const files = tree.files;
+    expect(files).toContain('/bar/angular.json');
+    expect(files).not.toContain('/bar/src');
+  });
 });
--- src/ng-new/schema.ts
+++ src/ng-new/schema.ts
@@ -1,27 +1,28 @@
 export interface Schema {
-    directory?: string;
-    name: string;
-    skipInstall?: boolean;
-    linkCli?: boolean;
-    skipGit?: boolean;
-    commit?: CommitUnion;
-    newProjectRoot?: string;
-    inlineStyle?: boolean;
-    inlineTemplate?: boolean;
-    viewEncapsulation?: string;
-    version: string;
-    routing?: boolean;
-    prefix?: string;
-    style?: string;
-    skipTests?: boolean;
-    experimentalIvy?: boolean;
-    createApplication?: boolean;
-    moduleName?: string;
+  directory?: string;
+  name: string;
+  enableIvy?: boolean;
+  skipInstall?: boolean;
+  linkCli?: boolean;
+  skipGit?: boolean;
+  commit?: CommitUnion;
+  newProjectRoot?: string;
+  inlineStyle?: boolean;
+  inlineTemplate?: boolean;
+  viewEncapsulation?: string;
+  version: string;
+  routing?: boolean;
+  prefix?: string;
+  style?: string;
+  skipTests?: boolean;
+  experimentalIvy?: boolean;
+  createApplication?: boolean;
+  moduleName?: string;
 }
 
 export declare type CommitUnion = boolean | CommitObject;
 export interface CommitObject {
-    email: string;
-    message?: string;
-    name: string;
+  email: string;
+  message?: string;
+  name: string;
 }
